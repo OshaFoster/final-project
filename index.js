5555f076cebf31eda56e50be720c6221cadab93c
Osha Foster <oshafoster@hotmail.com>
2017-12-07 09:45:38 -0700

    third deploy

--- index.js
+++ index.js
@@ -7,20 +7,20 @@ const cors = require("cors");
 const path = require("path")
 
 const port = process.env.PORT || 8008
+app.use(express.static(path.join(__dirname,"client", "build")))
 
 app.use(bodyParser.json());
 app.use(cors());
 
 app.use("/recipes", require("./routes/recipes"));
-app.use(express.static(path.join(__dirname,"client", "build")))
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/recipes", (err)=>{
     if(err) throw err;
     console.log("connected mongodb")
 })
 app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "build", "client", "index.html"))
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"))
 });
-app.listen(...)
+
 app.listen(port, ()=>{
     console.log(`app is listening${port}`)
 })
